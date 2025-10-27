5f5c91678d6f9e922f7433122b8b003373e7d129
fareguard <hello@fareguard.co.uk>
2025-10-27 17:57:38 +0000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,7 +5,6 @@ const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const service = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabase = createClient(url, anon);
-
 export const supabaseAdmin = createClient(url, service, {
   auth: { persistSession: false },
 });
