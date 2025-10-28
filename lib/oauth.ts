1303984fd7d78a632f57aa44b4169d253d4569e2
fareguard <hello@fareguard.co.uk>
2025-10-28 01:02:29 +0000

    Update oauth.ts

--- lib/oauth.ts
+++ lib/oauth.ts
@@ -1,8 +1,11 @@
 // lib/oauth.ts
-export function toQuery(params: Record<string,string>) {
-  const usp = new URLSearchParams(params);
-  return usp.toString();
+
+/** Convert an object into a querystring */
+export function toQuery(params: Record<string, string>) {
+  return new URLSearchParams(params).toString();
 }
+
+/** Generate an ISO timestamp now + N seconds */
 export function nowPlus(seconds: number) {
   return new Date(Date.now() + seconds * 1000).toISOString();
 }
